ebdec8c42e11a0bf3a5c6b6931143fb3ef8cc002
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-09 14:41:49 -0600

    Api added

--- src/Weather.js
+++ src/Weather.js
@@ -1,62 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [weatherCondition, setweatherData] = useState({ ready: false });
   function handleResponse(response) {
-    console.log(response.data);
+    setweatherData({
+      ready: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      time: response.data.timezone,
+      description: response.data.weather[0].description,
+      iconUrl:
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII=",
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
   }
 
-  let apiKey = process.env.REACT_APP_Weather_iD;
-  let city = "New York";
-  let uRl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  axios.get(uRl).then(handleResponse);
-  return (
-    <div className="Weather">
-      <h2 className="locate">New York</h2>
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="locate city here..."
-              className="form-control"
-              autoFocus="on"
+  if (weatherCondition.ready) {
+    return (
+      <div className="Weather">
+        <h2 className="locate">{weatherCondition.city}</h2>
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="locate city here..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-success w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <ul>
+          <li>date:{weatherCondition.date}</li>
+          <li>time:{weatherCondition.time}</li>
+          <li className="description">
+            forecast: {""}
+            {weatherCondition.description}
+          </li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              src={weatherCondition.iconUrl}
+              alt={weatherCondition.description}
+              className="float-left"
             />
+            <div className="float-end">
+              <span className="temp">
+                {Math.round(weatherCondition.temperature)}
+              </span>
+              <span className="unit">°C</span> | °F
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-outline-success w-100"
-            />
+          <div className="col-6">
+            <ul>
+              <li>Humidity:{weatherCondition.humidity}</li>
+              <li>Wind:{weatherCondition.wind}km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <ul>
-        <li>date:</li>
-        <li>time:</li>
-        <li>forecast:</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img
-            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAUhJREFUeNrt230NgzAQh2GkIAUJyJgMJCABCZNQKcxBHdyOpFkyEkYGV9oL75Lff2Rwz8pXe2tEpLlzGgAAAAAAAAAAYHsDg4+8Hp1m0ATNrJGNzGmbZdvOZN+lAFLRkyb+KHgvMX1H5wYgFR5OFL2VcATiMgA9uFbzzFD4Oss+2qoA9ID6k0P9yKnRVwGgBzJeWPg6Y1GAdIGSwpmKAFRS/C5CFoDCw/6v08EcIF3wpNL0WQHSrS5WDBDXt0hrgGfFxX+eE7IApCc8cZIuB0BwBBBMAZz9+l+jwApgcggwWQJEhwDRBMDp8P+cBhYAg2OAwQIgOAYIFgCzY4DZAkBcB4C7AywvcIwAAADgNsiDEI/CvAzxOsyECFNiTIoyLc7CCEtjLI6yPE6DBC0yNEnRJkejJK2yNEvTLs8fJgAAAAAAAADg1nkDlR7XfJiH1ggAAAAASUVORK5CYII="
-            alt="Mostly Cloudy"
-            className="float-left"
-          />
-          <div className="float-end">
-            <span className="temp">6</span>
-            <span className="unit">'C</span> | 'F
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity:</li>
-            <li>Wind:</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = process.env.REACT_APP_Weather_iD;
+    let city = "New York";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handleResponse);
+
+    return "The App is loading...";
+  }
 }

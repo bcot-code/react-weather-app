3398171bb143a83fe04ac9e754871b7738d6d1a9
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-08 18:51:51 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is by {""}{" "}
-        <a href="https://github.com/bcot-code">Barbie</a> and is {""}
-        <a
-          href="https://github.com/bcot-code/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is by {""}{" "}
+          <a href="https://github.com/bcot-code">Barbie</a> and is {""}
+          <a
+            href="https://github.com/bcot-code/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

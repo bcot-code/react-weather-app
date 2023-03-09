26cc2f1c3a46eed20975c2a731e3e7123d61b88e
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-08 18:12:11 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
+      <h1>Weather App</h1>
+      <footer>
+        This project is by {""}{" "}
+        <a href="https://github.com/bcot-code">Barbie</a> and is {""}
+        <a
+          href="https://github.com/bcot-code/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }

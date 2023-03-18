601b2059ed2c9db381f3d0282dcf490c8b93fe52
Barbie <78989054+bcot-code@users.noreply.github.com>
2023-03-17 21:53:38 -0500

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -28,6 +28,8 @@ export default function Forecast(props) {
                   <WeatherForDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
